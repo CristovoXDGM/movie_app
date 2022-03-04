3c0295562cc94fe5e8d70747a20082c096b7dd8d
Cristovão Farias <cristovoxdgm@gmail.com>
2022-03-04 13:52:58 -0300

    first commit

---
+++ lib/app/search/domain/entities/result_entity.dart
@@ -0,0 +1,14 @@
+class Result {
+  String categories;
+  String movieName;
+  String movieImage;
+  String movieRating;
+  String movieAccent;
+  Result({
+    required this.categories,
+    required this.movieName,
+    required this.movieImage,
+    required this.movieRating,
+    required this.movieAccent,
+  });
+}
