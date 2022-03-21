d94734d1923594d51e97859f07cf12156a2d445b
Cristovão Farias <cristovoxdgm@gmail.com>
2022-03-20 22:44:25 -0300

    Remove unescessary files

--- lib/app/modules/search/domain/repositories/get_movies_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:movie_app_fteam/app/modules/search/domain/entities/result_search.dart';
-import 'package:movie_app_fteam/app/modules/search/domain/errors/errors.dart';
-
-abstract class GetMoviesRepository {
-  Future<Either<SearchMoviesException, List<ResultSearchEntity>>> getMovies();
-}
--- lib/app/modules/search/infra/datasource/get_movies_datasource.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:movie_app_fteam/app/modules/search/domain/entities/result_search.dart';
-import 'package:movie_app_fteam/app/modules/search/domain/errors/errors.dart';
-import 'package:movie_app_fteam/app/modules/search/infra/models/result_search_movie_model.dart';
-
-abstract class GetMoviesDatasource {
-  Future<List<ResultSearchMovieModel>> getMovies();
-}
